eeae6b133bac410a2dac5e1d57d1709ea61c9247
ajay-chandravanshi <ajaystudy38@gmail.com>
2025-02-11 19:28:44 +0530

    new code

---
+++ 3.print nd fetch data in table/src/EmpDesign.jsx
@@ -0,0 +1,14 @@
+
+const EmpDesign=(props)=>{
+    return(
+        <>
+        <tr>
+            <td>{props.eno}</td>
+            <td>{props.nm}</td>
+            <td>{props.ct}</td>
+            <td>{props.sal}</td>
+        </tr>
+        </>
+    )
+}
+export default EmpDesign;
---
+++ 4.styling in react/src/App.jsx
@@ -0,0 +1,10 @@
+
+const App=()=>{
+  return(
+   
+    <>
+   <h1 style={{color:"blue",textAlign:"center",fontSize:"50px"}}>Welcome To Cybrom</h1>
+    </>
+  )
+}
+export default App;
