a85dbafb25543a173c86c19eeeb4587edede7f4b
ajay-chandravanshi <ajaystudy38@gmail.com>
2025-02-13 19:43:18 +0530

    new code

--- 4.styling in react/src/App.jsx
+++ 4.styling in react/src/App.jsx
@@ -1,9 +1,16 @@
-
+const data ={
+  color:"red",
+  textDecoration:"underline"
+}
 const App=()=>{
   return(
    
     <>
-   <h1 style={{color:"blue",textAlign:"center",fontSize:"50px"}}>Welcome To Cybrom</h1>
+   {/* <h1 style={{color:"blue",textAlign:"center",fontSize:"50px"}}>Welcome To Cybrom</h1> */}
+   <div style={data}>
+    we are react developer from bhopal
+   </div>
+   <h2>Hello friends we learn ReactJS</h2>
     </>
   )
 }
---
+++ 6.Set images in react/src/App.jsx
@@ -0,0 +1,19 @@
+import myimg1 from "./images/image1.jpeg";
+import myimg2 from "./images/image2.jpeg";
+import myimg3 from "./images/image3.jpeg";
+const App=()=>{
+  return(
+   
+    <>
+    <div>
+   <img src={myimg1} />
+   <img src={myimg2} />
+   <img src={myimg3} />
+   </div>
+   <div>
+    
+   </div>
+    </>
+  )
+}
+export default App;
