99cd42d9c8e8b6d7a188013616f67b5c66b3f44b
ajay-chandravanshi <ajaystudy38@gmail.com>
2025-02-14 23:27:00 +0530

    new code

---
+++ 14 feb/src/App.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Layout from "./layout";
+const App=()=>{
+  return(
+    <>
+     <BrowserRouter>
+     <Routes>
+      <Route path="/" element={<Layout/>} >
+      <Route path="*" element={<NoPage/>} />
+       <Route index element={<Home/>} />
+       <Route path="home" element={<Home/>} />
+       <Route path="about" element={<About/>} />
+       <Route path="contact" element={<Contact/>}/>
+      </Route>
+     </Routes>
+     </BrowserRouter>
+    </>
+  )
+}
+export default App;
---
+++ 14 feb/src/pages/NoPage.jsx
@@ -0,0 +1,9 @@
+
+const NoPage=()=>{
+    return(
+        <>
+        <h1>Error:404-No pge found</h1>
+        </>
+    )
+}
+export default NoPage;
