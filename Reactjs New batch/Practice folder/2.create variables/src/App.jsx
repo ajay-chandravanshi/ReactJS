032aa551859e678616ae3625c85115e3909b14ca
ajay-chandravanshi <ajaystudy38@gmail.com>
2025-03-31 01:52:23 +0530

    new code

---
+++ Reactjs New batch/Practice folder/2.create variables/src/App.jsx
@@ -0,0 +1,12 @@
+
+const name="Ajay Chandravanshi"
+const age=23
+const city="Bhopal"
+const App=()=>{
+    return(
+        <>
+        <h1>Hello Friends I am {name} , my age is {age} , I am from {city} </h1>
+        </>
+    )
+}
+export default App
