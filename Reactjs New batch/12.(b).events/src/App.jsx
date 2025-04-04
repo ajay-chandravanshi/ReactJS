e7999f3ab11f26f48a9479c8b41143166338e45d
ajay-chandravanshi <ajaystudy38@gmail.com>
2025-04-04 23:43:00 +0530

    new code

--- Reactjs New batch/12.(b).events/src/App.jsx
+++ Reactjs New batch/12.(b).events/src/App.jsx
@@ -1,14 +1,19 @@
 
 const App = () => {
-  const display=(e)=>{
-    alert(" Type: " + e.type + " Name: " + e.target.name + " Value: "+ e.target.value)
-  }
+  // const display=(e)=>{
+  //   alert(" Type: " + e.type + " Name: " + e.target.name + " Value: "+ e.target.value)
+  // }
+  // const display=(nm,e)=>{
+  //   alert(nm," Type: " + e.type + " Name: " + e.target.name + " Value: "+ e.target.value)
+  // }
 
 
   return (
     <>
     <h1>Welcome</h1>
-    <button name="btn1" value="myBtn" onClick={display}>Click Here</button>
+    <h1>This is the output</h1>
+    {/* <button name="btn1" value="myBtn" onClick={display}>Click Here</button> */}
+    {/* <button name="mybtn" value="btn0" onClick={(e)=>{display("Neeraj",e)}}>Click Here</button> */}
     </>
   );
 };
