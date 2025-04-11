7d5a2db067b58fbbc9eea96dc27a2896f631bfff
ajay-chandravanshi <ajaystudy38@gmail.com>
2025-04-11 23:58:49 +0530

    new code

--- Reactjs New batch/16.new/src/Display.jsx
+++ Reactjs New batch/16.new/src/Display.jsx
@@ -1,9 +1,13 @@
 
 const Display = () => {
-  return (
-   <>
-   </>
-  )
-}
+   const ans = myData.map((key) => {
+    <tr>
+    <td>{key.rollno}</td>
+    <td>{key.name}</td>
+    <td>{key.city}</td>
+    <td>{key.fees}</td>
+  </tr>
+   })
+  }  
 
 export default Display
